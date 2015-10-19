a69cfc2982382fda8f017e8d2448f0b5fe63b3f3
(๑• . •๑) <comcp@126.com>
2015-10-19 10:16:20 +0800

    改成链式调用

--- src/com/androidquery/callback/ImageOptions.java
+++ src/com/androidquery/callback/ImageOptions.java
@@ -10,7 +10,7 @@ public class ImageOptions {
 	public boolean fileCache = true;
 	public Bitmap preset;
 	public int policy;
-	
+
 	public int targetWidth;
 	public int fallback;
 	public int animation;
@@ -18,5 +18,86 @@ public class ImageOptions {
 	public int round;
 	public float anchor = AQuery.ANCHOR_DYNAMIC;
 
-	
+	public float getAnchor() {
+		return anchor;
+	}
+
+	public int getAnimation() {
+		return animation;
+	}
+
+	public int getFallback() {
+		return fallback;
+	}
+
+	public int getPolicy() {
+		return policy;
+	}
+
+	public Bitmap getPreset() {
+		return preset;
+	}
+
+	public float getRatio() {
+		return ratio;
+	}
+
+	public int getRound() {
+		return round;
+	}
+
+	public int getTargetWidth() {
+		return targetWidth;
+	}
+
+	public ImageOptions setAnchor(float anchor) {
+		this.anchor = anchor;
+		return this;
+	}
+
+	public ImageOptions setAnimation(int animation) {
+		this.animation = animation;
+		return this;
+	}
+
+	public ImageOptions setFallback(int fallback) {
+		this.fallback = fallback;
+		return this;
+	}
+
+	public ImageOptions setFileCache(boolean fileCache) {
+		this.fileCache = fileCache;
+		return this;
+	}
+
+	public ImageOptions setMemCache(boolean memCache) {
+		this.memCache = memCache;
+		return this;
+	}
+
+	public ImageOptions setPolicy(int policy) {
+		this.policy = policy;
+		return this;
+	}
+
+	public ImageOptions setPreset(Bitmap preset) {
+		this.preset = preset;
+		return this;
+	}
+
+	public ImageOptions setRatio(float ratio) {
+		this.ratio = ratio;
+		return this;
+	}
+
+	public ImageOptions setRound(int round) {
+		this.round = round;
+		return this;
+	}
+
+	public ImageOptions setTargetWidth(int targetWidth) {
+		this.targetWidth = targetWidth;
+		return this;
+	}
+
 }
